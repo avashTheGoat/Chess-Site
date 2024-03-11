09f7d4347d9c69f0f89d607f0d149d50e82d47b8
Avinash Devineni <devinenia69420@gmail.com>
2024-03-11 17:59:45 -0500

    Main html page works, images don't work

--- functions/main.js
+++ functions/main.js
@@ -9,15 +9,15 @@ const PORT = process.env.PORT || 3000;
 
 app.use('/', express.static(process.cwd() + '/public'));
 
-app.get('/engine_url.txt', (req, res) => {
+app.get('./engine_url.txt', (req, res) => {
     res.sendFile(process.cwd() + '/engine_url.txt');
 });
 
-app.get('/img/chesspieces/:piece', (req, res) => {
+app.get('./img/chesspieces/:piece', (req, res) => {
     res.sendFile(process.cwd() + `/img/chesspieces/${req.params.piece}`);
 });
 
-app.get('/fonts/Roboto-Regular.ttf', (req, res) => {
+app.get('./fonts/Roboto-Regular.ttf', (req, res) => {
     res.sendFile(process.cwd() + '/fonts/Roboto-Regular.ttf');
 });
 
